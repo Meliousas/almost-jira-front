040fe4b9b68742cf3f236acd9ca001fc8965a597
Meliousas <meliousas@gmail.com>
2018-06-19 01:24:20 +0200

    Scheme of the project - header, footer and home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+
+const appRoutes: Routes = [
+  // { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+   { path: '', component: HomeComponent },
+  // { path: 'login', component: LoginComponent },
+//  { path: 'register', component: RegisterComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
