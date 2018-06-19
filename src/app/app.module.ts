0f4c13c5bd85e2c38fbdd0c294edb6dbd73c393b
Meliousas <meliousas@gmail.com>
2018-06-19 23:07:05 +0200

    Files generated, routing active

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,17 @@ import { FooterComponent } from './home/footer/footer.component';
 import { HeaderComponent } from './home/header/header.component';
 import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
 
 const appRoutes: Routes = [
   // { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
    { path: '', component: HomeComponent },
-  // { path: 'login', component: LoginComponent },
-//  { path: 'register', component: RegisterComponent },
+   { path: 'login', component: UserComponent,
+     children: [{ path: '', component: SignInComponent} ]},
+   { path: 'register', component: UserComponent,
+     children: [{ path: '', component: SignUpComponent} ]},
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' }
@@ -22,7 +27,10 @@ const appRoutes: Routes = [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    UserComponent,
+    SignUpComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
